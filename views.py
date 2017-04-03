237fe6bd9ad2bf48d5abe8652b8564db5fc6863b
arewm <ajmcnama@ncsu.edu>
2017-04-03 17:27:59 -0400

    first dynamic page test

--- views.py
+++ views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.loader import loader
+
+from .models import Tag
 
 def index(request):
-	return HttpResponse('Hello, world. You\'re at the index. {}'.format(request))
+    return HttpResponse('Hello, world. You\'re at the index. {}'.format(request))
 
 def content(request):
-	return HttpResponse('Hello, you are at content. {}'.format(request))
+    return HttpResponse('Hello, you are at content. {}'.format(request))
 
 def workspace(request):
-	return HttpResponse('Hello, you are at workspace. {}'.format(request))
+    tag_list = Tag.objects.order_by('text')
+    #template = loader.get_template('survey/policy.html')
+    context = {'tags': tag_list}
+    return render(request, 'survey/policy.html', context)
+    #return HttpResponse('Hello, you are at workspace. {}'.format(request))
 
 def rank(request):
-	return HttpResponse('Hellow, you are at rank. {}'.format(request))
+    return HttpResponse('Hellow, you are at rank. {}'.format(request))
 # Create your views here.
