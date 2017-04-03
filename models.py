7ac46e716a53b7a38481a0a2232bebd358a5322c
arewm <ajmcnama@ncsu.edu>
2017-04-03 16:30:51 -0400

    add db to admin

--- models.py
+++ models.py
@@ -3,33 +3,39 @@ from django.db import models
 
 # Create your models here.
 
+
 class Person(models.Model):
-	expert_class = models.BooleanField(default=False)
-	person_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	consent_accepted = models.BooleanField(default=False)
+    expert_class = models.BooleanField(default=False)
+    person_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    consent_accepted = models.BooleanField(default=False)
+
 
 class Tag(models.Model):
-	tag_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	text = models.CharField(max_length=50)
-	tag_class = models.CharField(max_length=50)
-	custom = models.BooleanField(default=False)
-	creator = models.ForeignKey(Person, on_delete=models.CASCADE, default=None, null=True)
+    tag_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    text = models.CharField(max_length=50)
+    tag_class = models.CharField(max_length=50)
+    custom = models.BooleanField(default=False)
+    creator = models.ForeignKey(Person, on_delete=models.CASCADE, default=None, null=True)
+
 
 class Action(models.Model):
-	action_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	text = models.CharField(max_length=100)
+    action_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    text = models.CharField(max_length=100)
+
 
 class PolicyAction(models.Model):
-	action_id = models.ForeignKey(Action, on_delete=models.PROTECT)
-	allow = models.BooleanField()
+    action_id = models.ForeignKey(Action, on_delete=models.PROTECT)
+    allow = models.BooleanField()
+
 
 class PolicyTag(models.Model):
-	tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-	priority = models.IntegerField()
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    priority = models.IntegerField()
+
 
 class Policies(models.Model):
-	policy_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-	tags = models.ManyToManyField(PolicyTag)
-	actions = models.ManyToManyField(PolicyAction)
-	owner = models.ForeignKey(Person, on_delete=models.CASCADE)
-	time_to_generate = models.FloatField()
+    policy_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    tags = models.ManyToManyField(PolicyTag)
+    actions = models.ManyToManyField(PolicyAction)
+    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+    time_to_generate = models.FloatField()
