66f2b2a71e9259c30af733993e865d1a0a9b30b7
arewm <ajmcnama@ncsu.edu>
2017-04-05 21:49:54 -0400

    test new tag layout

--- static/survey/js/rank.js
+++ static/survey/js/rank.js
@@ -14,7 +14,7 @@
             //var dy = parseInt(event.target.getAttribute('data-y'), 10);
             event.target.setAttribute('blah-x', event.target.getAttribute('data-x'));
             event.target.setAttribute('blah-y', event.target.getAttribute('data-y'));
-            event.interactionw.x = event.target.getAttribute('data-x') === null ? window.getComputedStyle(event.target).left.slice(0, -2) : parseInt(event.target.getAttribute('data-x'), 10);
+            event.interaction.x = event.target.getAttribute('data-x') === null ? window.getComputedStyle(event.target).left.slice(0, -2) : parseInt(event.target.getAttribute('data-x'), 10);
             event.interaction.y = event.target.getAttribute('data-y') === null ? window.getComputedStyle(event.target).top.slice(0, -2) : parseInt(event.target.getAttribute('data-y'), 10);
         })
         .on('dragmove', function (event) {
