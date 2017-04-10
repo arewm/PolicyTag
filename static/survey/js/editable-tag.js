3b26bf7dcfecfe746470af03eea4e22b623743ec
arewm <ajmcnama@ncsu.edu>
2017-04-10 18:13:47 -0400

    test new tag layout

--- static/survey/js/editable-tag.js
+++ static/survey/js/editable-tag.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
                 // TODO finish this!
                 // TODO will have to move code up from lower down to create the policy tag
                 console.log(data);
-                var resp = jQuery.parseJSON(data);
+                var resp = data;
                 viewableText.html(resp.text);
                 viewableText.removeClass("write-in-me");
                 viewableText.addClass("move-me ui-draggable ui-draggable-handle");
