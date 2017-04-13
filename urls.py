63ecefcdf95741b54648c27a301b441ddc980a41
arewm <ajmcnama@ncsu.edu>
2017-04-13 07:52:18 -0400

    save ranking

--- urls.py
+++ urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     url(r'^submit_policy', views.submit_policy, name='submit_policy'),
     url(r'^custom_tag', views.custom_tag, name='custom_tag'),
 	url(r'^rank', views.rank, name='rank'),
+    url(r'^save_rank', views.save_rank, name='save_rank'),
 	url(r'^gen', views.gen, name='generator'),
-	url(r'^questions', views.survey, name='survey'),
-	url(r'^end', views.end, name='end'),
+	# url(r'^questions', views.survey, name='survey'),
+	# url(r'^end', views.end, name='end'),
 ]
