a71459857be888f1606b43516e2fe8cc2c24d92e
arewm <ajmcnama@ncsu.edu>
2017-04-15 10:23:51 -0400

    display suggested policies

--- urls.py
+++ urls.py
@@ -2,15 +2,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^consent', views.consent, name='consent'),
-	url(r'^tutorial', views.tutorial, name='tutorial'),
-	url(r'^policy', views.policy, name='policy'),
+    url(r'^$', views.index, name='index'),
+    url(r'^consent', views.consent, name='consent'),
+    url(r'^tutorial', views.tutorial, name='tutorial'),
+    url(r'^policy', views.policy, name='policy'),
     url(r'^submit_policy', views.submit_policy, name='submit_policy'),
     url(r'^custom_tag', views.custom_tag, name='custom_tag'),
-	url(r'^rank', views.rank, name='rank'),
+    url(r'^rank', views.rank, name='rank'),
     url(r'^save_rank', views.save_rank, name='save_rank'),
-	url(r'^gen', views.gen, name='generator'),
-	# url(r'^questions', views.survey, name='survey'),
-	url(r'^end', views.end, name='end'),
+    url(r'^gen', views.gen, name='generator'),
+    url(r'^thankyou', views.end, name='end'),
 ]
