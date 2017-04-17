a54653827226bafc3893cec1f01e87004aac9782
arewm <ajmcnama@ncsu.edu>
2017-04-17 14:35:50 -0400

    transition model

--- models.py
+++ models.py
@@ -24,7 +24,7 @@ class Person(models.Model):
 class Tag(models.Model):
     tag_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.CharField(max_length=50)
-    tag_cat = models.ForeignKey(TagCategory, on_delete=models.CASCADE)
+    tag_cat = models.ForeignKey(TagCategory, on_delete=models.CASCADE, null=True)
     custom = models.BooleanField(default=False)
     creator = models.ForeignKey(Person, on_delete=models.CASCADE, default=None, null=True)
 
